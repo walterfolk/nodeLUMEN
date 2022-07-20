09b0e0831907674361a44c09ec5ec59ce45b8fe3
Walter Folk <walter.folk@gmail.com>
2022-07-20 13:24:04 -0300

    dotenv y form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
-
+var nodemailer = require('nodemailer')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.post('/#contacto', function(req, res, next){
+  console.log(req.body)
+
+})
+
 
 module.exports = router;
