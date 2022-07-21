53c36becc8fbcf8d8c1aa7af84f9513905d8e27c
Walter Folk <walter.folk@gmail.com>
2022-07-21 17:44:51 -0300

    responsive

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.post('/#contacto', function(req, res, next){
+router.post('#contacto', function(req, res, next){
   console.log(req.body)
 
 })
